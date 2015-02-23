3b48bb626379bd1f6110f159bc278f84c3e2fe1d
Liam H <liamthanrahan@gmail.com>
2015-02-23 20:36:35 +1100

    Commit initial project files

---
+++ LiamonLimeWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LiamonLimeWebsite.Startup))]
+namespace LiamonLimeWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
